edbdfdb8acd4ecfde07658b7cbab1967db66e4e7
JumpSpinn <jumpspinn@gmx.de>
2026-03-12 23:31:17 +0100

    added vue project (web)

--- ExampleAspireBlazorApiProject.AppHost/AppHost.cs
+++ ExampleAspireBlazorApiProject.AppHost/AppHost.cs
@@ -1,12 +1,23 @@
 var builder = DistributedApplication.CreateBuilder(args);
+var vue = true;
 
 var apiService = builder.AddProject<Projects.ExampleAspireBlazorApiProject_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.ExampleAspireBlazorApiProject_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
+if (!vue)
+{
+    builder.AddProject<Projects.ExampleAspireBlazorApiProject_Web>("webfrontend")
+        .WithExternalHttpEndpoints()
+        .WithHttpHealthCheck("/health")
+        .WithReference(apiService)
+        .WaitFor(apiService);
+}
+else
+{
+    builder.AddNpmApp("vue-frontend", "../ExampleAspireBlazorApiProject.Vue", "dev")
+        .WithReference(apiService)
+        .WaitFor(apiService)
+        .WithHttpEndpoint(env: "PORT");
+}
 
 builder.Build().Run();
