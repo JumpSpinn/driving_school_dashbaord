a44422b88cb682d696a6e153186c67d413a48885
JumpSpinn <jumpspinn@gmx.de>
2026-03-13 10:32:51 +0100

    add base api client class

---
+++ ExampleAspireBlazorApiProject.Vue/src/apis/base/BaseApiClient.ts
@@ -0,0 +1,16 @@
+﻿import axios, {type AxiosInstance} from "axios";
+
+export class BaseApiClient {
+  public client: AxiosInstance;
+  public baseURL: string;
+
+  constructor() {
+    this.baseURL = "http://localhost:5514/api/";
+    this.client = axios.create({
+      baseURL: this.baseURL,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+}
