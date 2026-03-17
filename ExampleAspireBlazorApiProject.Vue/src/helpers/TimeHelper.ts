93164b2d495776ec1b28fd828ec4f8925837661e
JumpSpinn <jumpspinn@gmx.de>
2026-03-17 15:51:40 +0100

    add TimeHelper utility class

---
+++ ExampleAspireBlazorApiProject.Vue/src/helpers/TimeHelper.ts
@@ -0,0 +1,40 @@
+﻿export class TimeHelper {
+  static convert(timestamp: string | Date | null | undefined, format: 'full' | 'date' | 'time' | 'datetime' = 'full'): string {
+    if(timestamp == null) return "Keine Angabe";
+
+    const date = typeof timestamp === 'string' ? new Date(timestamp) : timestamp;
+
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: 'Europe/Berlin'
+    };
+
+    switch (format) {
+      case 'full':
+        options.dateStyle = 'full';
+        options.timeStyle = 'short';
+        break;
+      case 'date':
+        options.year = 'numeric';
+        options.month = '2-digit';
+        options.day = '2-digit';
+        break;
+      case 'time':
+        options.hour = '2-digit';
+        options.minute = '2-digit';
+        break;
+      case 'datetime':
+        options.year = 'numeric';
+        options.month = '2-digit';
+        options.day = '2-digit';
+        options.hour = '2-digit';
+        options.minute = '2-digit';
+        break;
+    }
+
+    return date.toLocaleString('de-DE', options);
+  }
+
+  public static async wait(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+}
