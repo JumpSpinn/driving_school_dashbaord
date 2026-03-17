f282f10e97968630e04d5b370ae0e25a4b9f5aca
JumpSpinn <jumpspinn@gmx.de>
2026-03-17 14:04:09 +0100

    add custom paper

---
+++ ExampleAspireBlazorApiProject.Vue/src/components/paper/CustomPaper.vue
@@ -0,0 +1,20 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="custom_paper">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.custom_paper{
+  margin: var(--size-16);
+  border-radius: var(--size-8);
+  background-color: var(--bg-light);
+  border: 1px solid var(--border);
+  padding: var(--size-16);
+  box-shadow: var(--shadow-dark);
+}
+</style>
