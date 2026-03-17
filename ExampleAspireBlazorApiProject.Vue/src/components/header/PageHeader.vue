e555b114a986d038d4593c8a67c77551dc37d1a2
JumpSpinn <jumpspinn@gmx.de>
2026-03-17 14:04:33 +0100

    add custom page header

---
+++ ExampleAspireBlazorApiProject.Vue/src/components/header/PageHeader.vue
@@ -0,0 +1,31 @@
+﻿<template>
+  <div class="page_header">
+    <div class="content">
+      <slot name="default" />
+      <div class="desc">
+        <slot name="description" />
+      </div>
+    </div>
+    <div class="actions">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.page_header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .desc{
+    color: var(--text-muted);
+  }
+
+  .actions{
+    display: flex;
+    align-items: center;
+    gap: var(--size-8);
+  }
+}
+</style>
