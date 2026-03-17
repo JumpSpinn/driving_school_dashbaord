e0e4bf5580165cf2fced6e72282def2d575ac3e8
JumpSpinn <jumpspinn@gmx.de>
2026-03-17 15:23:09 +0100

    add button group

---
+++ ExampleAspireBlazorApiProject.Vue/src/components/button/ButtonGroup.vue
@@ -0,0 +1,13 @@
+﻿<template>
+  <div class="button_group">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.button_group{
+  display: flex;
+  align-items: center;
+  gap: var(--size-8);
+}
+</style>
