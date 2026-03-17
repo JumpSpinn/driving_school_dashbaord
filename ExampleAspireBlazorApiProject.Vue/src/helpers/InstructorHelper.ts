f9516e8504a09b534d4305030d56e3c9af2d2702
JumpSpinn <jumpspinn@gmx.de>
2026-03-17 15:24:06 +0100

    add instructor helper static class

---
+++ ExampleAspireBlazorApiProject.Vue/src/helpers/InstructorHelper.ts
@@ -0,0 +1,8 @@
+﻿import type {IInstructor} from "@/interfaces/IInstructor.ts";
+
+export class InstructorHelper {
+  public static getOwnerName(instructor?: IInstructor) : string {
+    if(instructor == null) return "Kein Besitzer";
+    return `${instructor.firstName} ${instructor.lastName}`;
+  }
+}
