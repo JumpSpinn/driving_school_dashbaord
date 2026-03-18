64b323368efd3359c2597fd3b8ef1732a4713c16
JumpSpinn <jumpspinn@gmx.de>
2026-03-18 11:58:07 +0100

    implement auto import and components to vite config

--- ExampleAspireBlazorApiProject.Vue/vite.config.ts
+++ ExampleAspireBlazorApiProject.Vue/vite.config.ts
@@ -1,14 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      dirs: ['src/components'],
+      dts: 'src/components.d.ts',
+    }),
   ],
   resolve: {
     alias: {
