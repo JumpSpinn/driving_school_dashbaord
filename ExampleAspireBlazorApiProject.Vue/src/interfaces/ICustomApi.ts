f4139a819087e54291ac4f86811b204f4ce21f14
JumpSpinn <jumpspinn@gmx.de>
2026-03-18 13:25:57 +0100

    finished custom api interface methods for CRUD operations

--- ExampleAspireBlazorApiProject.Vue/src/interfaces/ICustomApi.ts
+++ ExampleAspireBlazorApiProject.Vue/src/interfaces/ICustomApi.ts
@@ -1,4 +1,12 @@
-﻿export interface ICustomApi {
+﻿import type {ICourseBookingUpdate, ICourseBookingBase} from "@/interfaces/ICourseBooking.ts";
+import type {IDrivingSchoolBase, IDrivingSchoolUpdate} from "@/interfaces/IDrivingSchool.ts";
+import type {IInstructorBase, IInstructorUpdate} from "@/interfaces/IInstructor.ts";
+import type {IStudentBase, IStudentUpdate} from "@/interfaces/IStudent.ts";
+import type {ITheoryLessonBase, ITheoryLessonUpdate} from "@/interfaces/ITheoryLesson.ts";
+
+export interface ICustomApi {
   getAll() : Promise<any>;
+  create(data: ICourseBookingBase | IDrivingSchoolBase | IInstructorBase | IStudentBase | ITheoryLessonBase) : Promise<any>;
+  update(data: ICourseBookingUpdate | IDrivingSchoolUpdate | IInstructorUpdate | IStudentUpdate | ITheoryLessonUpdate) : Promise<any>;
   delete(id: number) : Promise<boolean>;
 }
