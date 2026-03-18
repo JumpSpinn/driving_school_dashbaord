e382607813c771902b79eace28928d36f0970e2e
JumpSpinn <jumpspinn@gmx.de>
2026-03-18 10:54:02 +0100

    add validation rules helper

---
+++ ExampleAspireBlazorApiProject.Vue/src/helpers/ValidationRules.ts
@@ -0,0 +1,18 @@
+﻿export type ValidationRule = (value: string) => string | null;
+
+export const Rules = {
+  required: (): ValidationRule =>
+    (value) => !value?.trim() ? "Dieses Feld ist erforderlich" : null,
+
+  minLength: (min: number) : ValidationRule =>
+    (value) => value && value.length < min ? `Mindestens ${min} Zeichen erforderlich.` : null,
+
+  maxLength: (max: number): ValidationRule =>
+    (value) => value && value.length > max ? `Maximal ${max} Zeichen erlaubt.` : null,
+
+  email: (): ValidationRule =>
+    (value) => value && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? 'Ungültige E-Mail-Adresse.' : null,
+
+  pattern: (regex: RegExp, message: string): ValidationRule =>
+    (value) => value && !regex.test(value) ? message : null,
+}
