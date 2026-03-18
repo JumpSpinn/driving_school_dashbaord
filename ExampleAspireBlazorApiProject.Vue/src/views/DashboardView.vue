06de7ed9e77b005bf094038fc03ff8998b51ae37
JumpSpinn <jumpspinn@gmx.de>
2026-03-18 21:59:32 +0100

    DashboardView.vue: integrate charts and data tables for enhanced visualization and usability

--- ExampleAspireBlazorApiProject.Vue/src/views/DashboardView.vue
+++ ExampleAspireBlazorApiProject.Vue/src/views/DashboardView.vue
@@ -1,13 +1,16 @@
 ﻿<script setup lang="ts">
-import type {IDropdownItem} from "@/interfaces/IDropdownItem.ts";
-import {useField} from "@/composables/useField.ts";
-import {Rules} from "@/helpers/ValidationRules.ts";
-import {useForm} from "@/composables/useForm.ts";
 import {useDrivingSchoolStore} from "@/stores/drivingSchoolStore.ts";
 import {useStudentStore} from "@/stores/studentStore.ts";
 import {useInstructorStore} from "@/stores/instructorStore.ts";
 import {useTheoryLessonStore} from "@/stores/theoryLessonStore.ts";
 import {useCourseBookingStore} from "@/stores/courseBookingStore.ts";
+import { Pie, Line } from 'vue-chartjs';
+import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, ArcElement, LineElement, Title, Tooltip, Legend} from 'chart.js';
+import Vue3Datatable from '@bhplugin/vue3-datatable'
+import {TimeHelper} from "@/helpers/TimeHelper.ts";
+import {StudentHelper} from "@/helpers/StudentHelper.ts";
+import {TheoryLessonHelper} from "@/helpers/TheoryLessonHelper.ts";
+import { Building2, LucideGraduationCap, LibraryBig, BookCheck, UserStar } from "lucide-vue-next";
 
 const isLoading = ref(true);
 const drivingSchoolStore = useDrivingSchoolStore();
@@ -15,6 +18,7 @@ const studentStore = useStudentStore();
 const instructorStore = useInstructorStore();
 const theoryLessonStore = useTheoryLessonStore();
 const courseBookingStore = useCourseBookingStore();
+ChartJS.register(ArcElement, PointElement, CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend);
 
 onMounted(async () => {
   await loadAllData();
@@ -28,65 +32,237 @@ const loadAllData = async () => {
     theoryLessonStore.fetchAll(),
     courseBookingStore.fetchAll(),
   ]);
-
+  prepareCharts();
+  prepareTable();
   isLoading.value = false;
 }
 
-// Form tests
-const dropdownItems = ref<IDropdownItem[]>([
-  { label: "Option 1", value: "1" },
-  { label: "Option 2", value: "2" },
-  { label: "Option 3", value: "3" },
-])
+// Charts
+const chartDefaultOptions = {
+  responsive: true,
+  maintainAspectRatio: true
+}
 
-const username = useField([
-  Rules.required(),
-  Rules.minLength(3),
-  Rules.maxLength(50),
-])
+const donutPassedData = ref({
+  labels: ["Bestanden", "in Ausbildung"],
+  datasets: [
+    {
+      backgroundColor: ['rgb(21, 213, 85)', 'rgb(235, 125, 52)'],
+      data: [0,0]
+    }
+  ]
+})
 
-const category = useField([
-  Rules.required(),
-])
+const donutDrivingSchoolData = ref({
+  labels: ["Zugewiesen", "ohne Zuweisung"],
+  datasets: [
+    {
+      backgroundColor: ['rgb(21, 213, 85)', 'rgb(235, 125, 52)'],
+      data: [0,0]
+    }
+  ]
+})
 
-const { validate, reset } = useForm([username, category]);
+const lineData = {
+  labels: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+  datasets: [
+    {
+      label: 'Buchungen',
+      backgroundColor: 'rgba(253, 73, 168, 1)',
+      borderColor: 'rgba(253, 73, 168, .3)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }
+  ]
+}
 
-const handleSubmit = () => {
-  if(!validate()) {
-    console.log("Validation failed");
-    return;
+const prepareCharts = () => {
+  // Data (Passed, Non-Passed)
+  donutPassedData.value.datasets[0]!.data[0] = studentStore.data.filter(s => s.hasPassed).length;
+  donutPassedData.value.datasets[0]!.data[1] = studentStore.data.filter(s => !s.hasPassed).length;
+
+  // Data (Driving School, Non-Driving School)
+  donutDrivingSchoolData.value.datasets[0]!.data[0] = studentStore.data.filter(s => s.drivingSchool).length;
+  donutDrivingSchoolData.value.datasets[0]!.data[1] = studentStore.data.filter(s => !s.drivingSchool).length;
+
+  // Data (Booking per Day)
+  const dayMap: Record<number, number> = {
+    1: 0,
+    2: 1,
+    3: 2,
+    4: 3,
+    5: 4,
+    6: 5,
+    0: 6,
   }
 
-  console.log("Benutzername: ", username.value);
-  console.log("Kategorie: ", category.value);
+  lineData.datasets[0]!.data = [0, 0, 0, 0, 0, 0, 0];
+
+  for(const booking of courseBookingStore.data){
+    if(!booking.bookingDate) return;
+
+    const dayIdx = dayMap[new Date(booking.bookingDate).getDay()];
+    if(dayIdx !== undefined)
+      lineData.datasets[0]!.data[dayIdx]!++;
+  }
+}
+
+// Table
+const cols = ref([
+  { field: "bookingDate", title: "Buchungsdatum" },
+  { field: "student", title: "Fahrschüler" },
+  { field: "theoryLesson", title: "Kurs" }
+]);
+const rows = ref<any[]>([]);
+
+const prepareTable = () => {
+  const data = [];
+  const bookings = courseBookingStore.data;
+  bookings.sort((a,b) => a.bookingDate! > b.bookingDate! ? -1 : 1);
+  bookings.splice(5, bookings.length - 5);
+
+  for(const booking of bookings) {
+    data.push({
+      bookingDate: TimeHelper.convert(booking.bookingDate, "date"),
+      student: StudentHelper.getFullName(booking.student),
+      theoryLesson: TheoryLessonHelper.getName(booking.theoryLesson),
+    })
+  }
+
+  rows.value = data;
 }
 
 </script>
 
 <template>
+  <div class="grid">
+    <CustomPaper class="paper">
+      <PageHeader>
+        <div class="header">
+          <h3>Fahrschulen</h3>
+          <Building2 :size="50" :stroke-width="1" class="icon" />
+        </div>
+        <h1>{{ drivingSchoolStore.data.length }}</h1>
+      </PageHeader>
+    </CustomPaper>
+
+    <CustomPaper class="paper">
+      <PageHeader>
+        <div class="header">
+          <h3>Fahrlehrer</h3>
+          <UserStar :size="50" :stroke-width="1" class="icon" />
+        </div>
+        <h1>{{ instructorStore.data.length }}</h1>
+      </PageHeader>
+    </CustomPaper>
+
+    <CustomPaper class="paper">
+      <PageHeader>
+        <div class="header">
+          <h3>Fahrschüler</h3>
+          <LucideGraduationCap :size="50" :stroke-width="1" class="icon" />
+        </div>
+        <h1>{{ studentStore.data.length }}</h1>
+      </PageHeader>
+    </CustomPaper>
+
+    <CustomPaper class="paper">
+      <PageHeader>
+        <div class="header">
+          <h3>Kurse</h3>
+          <LibraryBig :size="50" :stroke-width="1" class="icon" />
+        </div>
+        <h1>{{ theoryLessonStore.data.length }}</h1>
+      </PageHeader>
+    </CustomPaper>
+
+    <CustomPaper class="paper">
+      <PageHeader>
+        <div class="header">
+          <h3>Kurs-Buchungen</h3>
+          <BookCheck :size="50" :stroke-width="1" class="icon" />
+        </div>
+        <h1>{{ courseBookingStore.data.length }}</h1>
+      </PageHeader>
+    </CustomPaper>
+  </div>
+
+  <div class="wrapper">
+    <CustomPaper class="paper chart">
+      <Line :data="lineData" :options="chartDefaultOptions" v-if="!isLoading" />
+    </CustomPaper>
+    <CustomPaper class="paper chart">
+      <Pie :data="donutDrivingSchoolData" :options="chartDefaultOptions" v-if="!isLoading" />
+    </CustomPaper>
+    <CustomPaper class="paper chart">
+      <Pie :data="donutPassedData" :options="chartDefaultOptions" v-if="!isLoading" />
+    </CustomPaper>
+  </div>
+
   <CustomPaper>
     <PageHeader>
-      <h3>Dashboard</h3>
+      <h3>Letzten Buchungen</h3>
     </PageHeader>
+    <Vue3Datatable
+      :loading="isLoading"
+      noDataContent="Es wurden noch keine Buchungen vorgenommen."
+      :rows="rows"
+      :columns="cols"
+      :showPageSize="false"
+      :showNumbers="false"
+      :showNumbersCount="false"
+      paginationInfo="Es werden die Einträge {0} bis {1} von {2} angezeigt."
+     />
   </CustomPaper>
-
-  <Modal :open="false">
-    <template #header>Input Test</template>
-    <template #content>
-      <form @submit.prevent="handleSubmit">
-        <CustomTextInput label="Benutzername:" :required="true" id="username" v-model="username.value" :error="username.error" />
-        <CustomDropdown label="Kategorie wählen:" :options="dropdownItems" v-model="category.value" :required="true" :error="category.error" />
-      </form>
-    </template>
-    <template #actions>
-      <ButtonGroup>
-        <CustomButton type="primary" @click="handleSubmit">Senden</CustomButton>
-      </ButtonGroup>
-    </template>
-  </Modal>
 
 </template>
 
 <style scoped>
+.grid{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  margin-bottom: -16px;
+}
 
+.wrapper{
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: -16px;
+}
+
+.paper{
+  flex-grow: 1;
+  margin-right: 0;
+
+  &:last-child{
+    margin-right: var(--size-16);
+  }
+
+  &.chart{
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
+}
+
+h1{
+  width: fit-content;
+  min-width: 50px;
+  color: var(--primary-color);
+  margin: unset;
+}
+
+.header{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: var(--size-4);
+
+  .icon{
+    margin-top: calc(var(--size-8) * -1);
+    opacity: .2;
+  }
+}
 </style>
