9d386df2901e0e5df9ae37bd12fac536914890c3
JumpSpinn <jumpspinn@gmx.de>
2026-03-18 10:54:48 +0100

    implement useForm composable to validate all fields and reset all fields

---
+++ ExampleAspireBlazorApiProject.Vue/src/composables/useForm.ts
@@ -0,0 +1,17 @@
+﻿export function useForm(fields: { validate: () => boolean }[]) {
+  const validate = (): boolean => {
+    const results = fields
+      .map((field) =>
+        field.validate());
+
+    return results.every(Boolean);
+  };
+
+  const reset = () =>
+    fields.forEach((f) => ('reset' in f ? (f as any).reset() : null));
+
+  return {
+    validate,
+    reset
+  };
+}
